3ec6c216a0bcf3d609069a110b02f82410ee6863
Rizky Ikhwan <rizkyikhwan.ri@gmail.com>
2022-01-01 17:34:22 +0700

    try to fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,13 @@
       keep-alive
       :keep-alive-props="{
         exclude: [
-          'components/Pagination.vue',
+          'Pagination',
           'pages/search.vue',
           'pages/movie/category/index.vue',
           'pages/movie/all-movies/index.vue',
           'pages/tv/category/index.vue',
           'pages/tv/all-series/index.vue'
-        ],
+        ]
       }"
     />
     <Footer />
