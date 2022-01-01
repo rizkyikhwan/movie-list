52469c7c9c52546bd5ae5f24e6aac0795cca23db
Rizky Ikhwan <rizkyikhwan.ri@gmail.com>
2022-01-01 19:39:20 +0700

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,7 @@
 <template>
   <main>
     <Navbar />
-    <Nuxt
-      keep-alive
-      :keep-alive-props="{
-        exclude: [
-          'components/Pagination.vue',
-          'pages/search.vue',
-          'pages/movie/category/index.vue',
-          'pages/movie/all-movies/index.vue',
-          'pages/tv/category/index.vue',
-          'pages/tv/all-series/index.vue'
-        ],
-      }"
-    />
+    <Nuxt />
     <Footer />
   </main>
 </template>
